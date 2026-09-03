5f082cbc05fe8b3ea0ca208c239ca02d401390cd
TheNax110 <lacuadrafrancisco652@gmail.com>
2026-09-03 19:00:09 -0300

    Delete js directory

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://ylgcmtmhdimiidsyazgt.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsZ2NtdG1oZGltaWlkc3lhemd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyMDc5OTcsImV4cCI6MjEwMzc4Mzk5N30.hUF6pxRob31_qNEJae91vqhFpT4ctINSDMJagcxlpM0";
-
-if (SUPABASE_URL === "TU_SUPABASE_URL" || SUPABASE_ANON_KEY === "TU_SUPABASE_ANON_KEY") {
-  console.warn(
-     
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
